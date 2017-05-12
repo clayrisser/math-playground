5507668fd28c20a78c06418303a839e2db8343cd
Jam Risser <jam@jamrizzi.com>
2017-05-11 23:17:04 -0500

    Started sigmoid

--- plot.py
+++ plot.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
 import sys
-from plots import exponential
+from plots import exponential, sigmoid
 
 def main():
     command = sys.argv[len(sys.argv) - 1]
     if (command == 'exponential'):
         exponential.plot()
+    if (command == 'sigmoid'):
+        sigmoid.plot()
     else:
         print('Invalid command')
         exit(1)
---
+++ plots/sigmoid.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(12)
+y = x
+
+plt.plot(x, y, 'o')
+
+def plot():
+    plt.show()
